340486957ec834b642513cdf4126259e95ade5a1
GhwFluffy <ghw@ghwfluffy.com>
2024-02-02 21:53:49 -0600

    Adding multi-touch zoom and max zoom

--- tavern/Zoom.gd
+++ tavern/Zoom.gd
@@ -1,5 +1,6 @@
 extends Node
 
+var DEBUG = null;
 var camera = null
 
 var last_drag_distance = -1  # Used for pinch zoom
@@ -10,6 +11,7 @@ var touch_2 = null
 # Called when the node enters the scene tree for the first time.
 func _ready():
     camera = get_tree().get_root().get_node("Tavern/Camera2D")
+    DEBUG = get_tree().get_root().get_node("Tavern/HUD/DEBUG")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -23,24 +25,43 @@ func handle_zoom(event):
         elif event.button_index == MOUSE_BUTTON_WHEEL_UP:
             camera.zoom += Vector2(0.01, 0.01)
 
+    if event is InputEventMagnifyGesture:
+        var zoom_change = (1.0 - event.factor) * 0.25
+        camera.zoom += Vector2(zoom_change, zoom_change)
+
     # Handle pinch zoom
     if event is InputEventScreenDrag:
         if event.index == 0:  # First touch point
             touch_1 = event.position
         elif event.index == 1:  # Second touch point
-            touch_2 = get_viewport().get_mouse_position()  # Assuming the second touch is where the mouse currently is
+            touch_2 = event.position
 
+        if touch_1 and touch_2:
             var current_distance = touch_1.distance_to(touch_2)
             if last_drag_distance != -1:
                 var zoom_change = (current_distance - last_drag_distance) * 0.01
                 camera.zoom += Vector2(zoom_change, zoom_change)
-
             last_drag_distance = current_distance
 
     # Reset pinch zoom tracking on release
     if event is InputEventScreenTouch and event.is_pressed() == false:
-        last_drag_distance = -1
+        touch_1 = null;
+        touch_2 = null;
+        last_drag_distance = null;
+    elif event is InputEventScreenTouch and event.is_pressed() == true:
+        if event.index == 0:  # First touch point
+            touch_1 = event.position
+        elif event.index == 1:  # Second touch point
+            touch_2 = event.position
 
+    if camera.zoom[0] > 2.0:
+        camera.zoom = Vector2(2.0, 2.0)
+    elif camera.zoom[0] < 0.3:
+        camera.zoom = Vector2(0.3, 0.3)
 
-func _on_background_area_input_event(viewport, event, shape_idx):
+#func _on_background_area_input_event(viewport, event, shape_idx):
+    #handle_zoom(event)
+
+func _input(event):
+    DEBUG.text = str(event)
     handle_zoom(event)
